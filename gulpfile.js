a23c7ef94de6b7790dc726b4fe3cb58707349a74
Advaith Doosa <advaithseo@gmail.com>
2017-07-29 19:31:42 +0530

    bootstrap, typedjs fullpage, animatecss, gulpfile for live reload

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp')
+var bs = require('browser-sync').create()
+
+gulp.task('browser-sync', function () {
+  bs.init({
+    server: {
+      baseDir: './'
+    }
+  })
+})
+
+gulp.task('watch', ['browser-sync'], function () {
+  gulp.watch('js/*.js').on('change', bs.reload)
+  gulp.watch('css/*.css').on('change', bs.reload)
+  gulp.watch('*.html').on('change', bs.reload)
+})
